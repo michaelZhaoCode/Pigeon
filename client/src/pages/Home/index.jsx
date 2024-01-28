148f5e1db126734f19e67a54c70cbf49048fa152
Vincent Zhu <vincenthero16@gmail.com>
2024-01-27 20:28:59 -0500

    Poscard history

--- client/src/pages/Home/index.jsx
+++ client/src/pages/Home/index.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useProfileVisibility } from "pages/MyProfile/userProfileVisibility";
 import Sidebar1 from "components/Sidebar1";
+import PostCard from "components/PostCard";
 import MyProfilePage from "pages/MyProfile";
 // import { Button, Img, Input, List, Text } from "components";
 
@@ -12,7 +13,10 @@ const HomePage = () => {
       <div className="bg-gray-100 flex sm:flex-col md:flex-col flex-row font-inter sm:gap-5 md:gap-5 items-start mx-auto w-full">
         <Sidebar1 className="!sticky !w-[165px] flex h-screen md:hidden justify-start overflow-auto md:px-5 top-[0]" />
         <div className="flex flex-1 flex-col items-center justify-end md:ml-[0] ml-[65px] md:mt-0 mt-10 md:px-5 w-full">
-          <div className="flex flex-col gap-10 items-center justify-start w-full"></div>
+          <div className="flex flex-col gap-5 items-center justify-center w-[50%]">
+            <PostCard></PostCard>
+            <PostCard></PostCard>
+          </div>
         </div>
       </div>
       <MyProfilePage ref={profileRef} isVisible={showProfile} />
